963661d048c3f7b771f5b1b098a4622d376eea69
Nidhin Raj <nidhin.raj@ihstowers.com>
2022-07-30 19:23:43 +0400

    Nidhin Changes

---
+++ main.py
@@ -0,0 +1,5 @@
+from summation import sum_
+
+x = int(input("Enter a numerical value"))
+y=  int(input("Enter a numerical value"))
+print(f"Sumamtion of {x} and {y} is {sum(x.y)}")
---
+++ summation.py
@@ -0,0 +1,5 @@
+def sum(x,y):
+    """
+    Summing two compatible values
+    """
+    return x+y;
